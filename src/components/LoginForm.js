c5825a63ce112a6e41272fb7761fea8c65ed338b
Jorge <jorgeemejia@csu.fullerton.edu>
2023-05-11 13:16:42 -0700

    digital signature verification works!

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import forge from "node-forge";
 
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [showForm, setShowForm] = useState("");
   const [order, setOrder] = useState("");
-  const [publicKey, setPublicKey] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
   const [jwt, setJwt] = useState("");
 
   const handleUsernameChange = (event) => {
@@ -19,6 +20,10 @@ function LoginForm() {
 
   const handleOrderChange = (event) => {
     setOrder(event.target.value);
+  }
+
+  const handlePrivateKeyChange = (event) => {
+    setPrivateKey(event.target.value);
   }
 
   const handleSubmit = (event) => {
@@ -39,12 +44,48 @@ function LoginForm() {
         console.log(error)
       })
   };
+
+
   const handleSubmit2 = (event) => {
     event.preventDefault();
-    console.log("Order: ", order);
     const timestamp = new Date().toISOString();
-    console.log("Timestamp: ", timestamp )
+    const pk = forge.pki.privateKeyFromPem(privateKey);
+    const md = forge.md.sha256.create();
+    md.update(order, 'utf8');
+    console.log("Transformed Order: ", md.digest().toHex());
+
+
+    const signature = pk.sign(md, 'RSASSA-PKCS1-V1_5', { md: forge.md.sha256.create() }); // Use the padded value for signing
+    const signatureBase64 = forge.util.encode64(signature);
+
+    console.log("timestamp: ", timestamp)
+    console.log("privateKey: ", privateKey)
+    console.log("signature: ", signature)
+    console.log("signatureBase64: ", signatureBase64)
+    console.log("jwt: ", jwt)
+    console.log("order: ", order)
+  
+    const headers = {
+      Authorization: `Bearer ${jwt}`, // Include the JWT token in the Authorization header
+    };
+  
+    const data = {
+      order: order,
+      timestamp: timestamp,
+      signature: signatureBase64, // Include the Base64-encoded signature
+    };
+  
+    axios
+      .post("https://localhost:3001/order", data, { headers })
+      .then((response) => {
+        // Handle the response
+        console.log('Response:', response.data.success);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+  
 
   return (
     <div>
@@ -68,6 +109,10 @@ function LoginForm() {
             <label>
               Order:
               <input type="text" value={order} onChange={handleOrderChange}/>
+            </label>
+            <label>
+              Private Key:
+              <input type="text" value={privateKey} onChange={handlePrivateKeyChange}/>
             </label>
             <button type="submit">Submit</button>
           </form>
